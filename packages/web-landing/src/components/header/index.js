f5dfe017e3b1ac1f63234fb39ff34a105e9d2651
Caio Kinzel Filho <caiokf@gmail.com>
2019-11-19 14:41:22 +0200

    moving web-landing to styled-components

--- packages/web-landing/src/components/header/index.js
+++ packages/web-landing/src/components/header/index.js
@@ -1,15 +1,56 @@
 import { h } from 'preact'
-import { Link } from 'preact-router/match'
-import style from './style.css'
+import styled from 'styled-components'
 
 const Header = () => (
-	<header class={style.header}>
-		<h1>Streek</h1>
-		<nav>
-			<Link href="/">Log in</Link>
-			<Link href="/">Sign up</Link>
-		</nav>
-	</header>
+	<Container>
+		<AppTitle>Streek</AppTitle>
+		<Menu>
+			<MenuLink href="http://localhost:3000/login">Log in</MenuLink>
+			<MenuLink href="http://localhost:3000/signup">Sign up</MenuLink>
+		</Menu>
+	</Container>
 )
 
+const Container = styled.header`
+	position: fixed;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 56px;
+	padding: 0;
+	background: transparent;
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+	z-index: 50;
+	font-weight: bolder;
+`
+const AppTitle = styled.h1`
+	float: left;
+	margin: 0;
+	padding: 0 15px;
+	font-size: 24px;
+	line-height: 56px;
+	font-weight: 400;
+	color: white;
+`
+const Menu = styled.nav`
+	float: right;
+	font-size: 100%;
+`
+const MenuLink = styled.a`
+	display: inline-block;
+	height: 56px;
+	line-height: 56px;
+	padding: 0 15px;
+	min-width: 50px;
+	text-align: center;
+	background: rgba(255,255,255,0);
+	text-decoration: none;
+	color: #FFF;
+	will-change: background-color;
+	cursor: pointer;
+	
+	&:hover {
+		background: rgba(0,0,0,0.2);
+	}
+`
 export default Header
--- packages/web-landing/src/routes/home/index.js
+++ packages/web-landing/src/routes/home/index.js
@@ -1,16 +1,51 @@
 import { h } from 'preact'
-import style from './style'
+import styled from 'styled-components'
 
 const Home = () => (
   <div>
-    <div class={style.background} />
-    <div class={style.overlay} />
-    <div class={style.content}>
-      <div class={style.appname}>
+    <Background />
+    <Overlay />
+    <Content>
+      <AppName>
         Streek
-      </div>
-    </div>
+      </AppName>
+    </Content>
   </div>
 )
+
+const Background = styled.div`
+  background: url('/assets/streek-metrics-tracker-running.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 0;
+`
+const Overlay = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 1;
+  background-image: linear-gradient(120deg, rgba(132, 250, 176, 1) 20%, rgba(143, 211, 244, 0.5) 70%);
+`
+const Content = styled.div`
+  z-index: 2;
+  font-family: 'Helvetica Neue';
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 70vh;
+  width: 40vw;
+  padding: 100px;
+`
+const AppName = styled.div`
+  font-size: 64px;
+  font-weight: bold;
+  color: white;
+`
 
 export default Home
